e742af96c1784b3a5807e59ae0a18deca07466d2
federico <pontonio.federico@gmail.com>
2024-04-23 18:50:03 +0200

    First commit
    all configuration prepared are tracked in the "notes.txt" file.

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "index_bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  // mode: 'development',      //to enable when needed
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
